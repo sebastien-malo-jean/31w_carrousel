8a2f1511d79016184c9d1584d43e035135c4b6d0
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2024-11-07 11:31:46 -0500

    Ajout de la page carrousel.php, les informations du plugin & la fonction pour généré le carrousel sont fait.

---
+++ carrousel.php
@@ -0,0 +1,31 @@
+<?php
+    /*
+    plugin name: 31w Carrousel
+    description: exercice permettant d'afficher le contenue d'une galerie dans un carrousel.
+    author: Sébastien Malo Jean
+    author uri: https://github.com/sebastien-malo-jean
+    */
+
+    /*
+    Instruction :
+    - généré le code HTML d'une partie de la boite modale
+    - créé un bourton qui permettra d'ouvrir la boite modale
+    - créer une feuille de style pour la boite modale
+    - Script permettant d'ouvrir et fermer la boite modale.
+    */
+
+function genere_carrousel() {
+
+    $chaine = '
+<button id="carrousel__button" class="carrousel__button">Ouvrir</button>
+<div class="carrousel">
+    <button class="carrousel__x">X</button>
+    <figure class="carrousel__figure"></figure>
+    <form class="carrousel__form"></form>
+</div>
+    ';
+
+    return $chaine;
+}
+
+add_shortcode("carrousel", "genere_carrousel");
